6e5a5f93db41c6ad8b339c6d4224c8eff1284907
Petru-Mihai Craciun <petru.uk@gmail.com>
2024-11-18 15:03:30 +0000

    game

--- Cozfee/Intermediate/Build/Win64/UE4Editor/Inc/Cozfee/Cozfee.init.gen.cpp
+++ Cozfee/Intermediate/Build/Win64/UE4Editor/Inc/Cozfee/Cozfee.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeCozfee_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x8F2E6CB7,
-				0x5D05555B,
+				0xD2815AFB,
+				0x27C8807B,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
